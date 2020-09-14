be78ba63eec30d426632b7ffd2823286fc28527b
Michel Koyang-Kuleshov <michelkoyangkuleshov@gmail.com>
2020-09-14 22:17:31 +0300

    Done task 1.5

--- hw_1/task_1_5.py
+++ hw_1/task_1_5.py
@@ -9,3 +9,64 @@
 а главная функция — общую сумму по вкладу на конец периода.
 """
 
+small = {
+    'begin_sum': 1000,
+    'end_sum': 10000,
+    6: 0.05 / 2,
+    12: 0.06,
+    24: 0.05
+}
+medium = {
+    'begin_sum': 10000,
+    'end_sum': 100000,
+    6: 0.06 / 2,
+    12: 0.07,
+    24: 0.065
+}
+high = {
+    'begin_sum': 100000,
+    'end_sum': 1000000,
+    6: 0.07 / 2,
+    12: 0.08,
+    24: 0.075
+}
+
+
+def get_deposit_sum(first_deposit, countdown, add_sum):
+    def add_deposit(add_sum, proc):
+        nonlocal countdown
+        count = countdown - 2
+        add_depo = add_sum * count + (add_sum * count * proc / countdown)
+        return add_depo
+
+    spam = 0
+    if first_deposit < small['end_sum']:
+        if countdown < 24:
+            end_deposit = (first_deposit + (first_deposit * small[countdown]) +
+                           add_deposit(add_sum, small[countdown]))
+        else:
+            spam += first_deposit + (first_deposit * small[countdown])
+            end_deposit = (spam + (spam * small[countdown]) +
+                           add_deposit(add_sum, small[countdown]))
+    elif first_deposit < medium['end_sum']:
+        if countdown < 24:
+            end_deposit = (first_deposit + (first_deposit * medium[countdown]) +
+                           add_deposit(add_sum, medium[countdown]))
+        else:
+            spam += first_deposit + (first_deposit * medium[countdown])
+            end_deposit = (spam + (spam * medium[countdown]) +
+                           add_deposit(add_sum, medium[countdown]))
+    else:
+        if countdown < 24:
+            end_deposit = (first_deposit + (first_deposit * high[countdown]) +
+                           add_deposit(add_sum, high[countdown]))
+        else:
+            spam += first_deposit + (first_deposit * high[countdown])
+            end_deposit = (spam + (spam * high[countdown]) +
+                           add_deposit(add_sum, high[countdown]))
+    return end_deposit
+
+
+print(get_deposit_sum(5000, 24, 100))
+print(get_deposit_sum(50000, 12, 1000))
+print(get_deposit_sum(500000, 24, 10000))
