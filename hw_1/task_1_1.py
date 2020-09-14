208c53a3d01250a63d7779952578b2b277a97301
Michel Koyang-Kuleshov <michelkoyangkuleshov@gmail.com>
2020-09-14 15:40:00 +0300

    Изменения, которые будут включены в коммит: изменено:      hw_1/task_1_1.py

--- hw_1/task_1_1.py
+++ hw_1/task_1_1.py
@@ -7,3 +7,19 @@
 главной функции. Элементы строки (элементы таблицы умножения) должны
 разделяться табуляцией.
 """
+
+
+def get_table_results(a, b):
+    for row in range(a + 1):
+        row_lst = list()
+        for col in range(b + 1):
+            if row == 0:
+                row_lst.append(col)
+            elif col == 0:
+                row_lst.append(row)
+            else:
+                row_lst.append(row * col)
+        print("\t".join([str(i) for i in row_lst]))
+
+
+get_table_results(5, 5)
