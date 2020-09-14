3af491b0ad697956f4e9e49370e1bcec515ff28d
Michel Koyang-Kuleshov <michelkoyangkuleshov@gmail.com>
2020-09-14 21:59:55 +0300

    Done task 1.4

--- hw_1/task_1_4.py
+++ hw_1/task_1_4.py
@@ -14,3 +14,53 @@
 принадлежность суммы вклада к одному из диапазонов и выполнять расчет по нужной
 процентной ставке. Функция возвращает сумму вклада на конец срока.
 """
+
+small = {
+    'begin_sum': 1000,
+    'end_sum': 10000,
+    6: 0.05 / 2,
+    12: 0.06,
+    24: 0.05
+}
+medium = {
+    'begin_sum': 10000,
+    'end_sum': 100000,
+    6: 0.06 / 2,
+    12: 0.07,
+    24: 0.065
+}
+high = {
+    'begin_sum': 100000,
+    'end_sum': 1000000,
+    6: 0.07 / 2,
+    12: 0.08,
+    24: 0.075
+}
+
+
+def get_deposit_sum(first_deposit, countdown):
+    spam = 0
+    if first_deposit < small['end_sum']:
+        if countdown < 24:
+            end_deposit = first_deposit + (first_deposit * small[countdown])
+        else:
+            spam += first_deposit + (first_deposit * small[countdown])
+            end_deposit = spam + (spam * small[countdown])
+    elif first_deposit < medium['end_sum']:
+        if countdown < 24:
+            end_deposit = first_deposit + (first_deposit * medium[countdown])
+        else:
+            spam += first_deposit + (first_deposit * medium[countdown])
+            end_deposit = spam + (spam * medium[countdown])
+    else:
+        if countdown < 24:
+            end_deposit = first_deposit + (first_deposit * high[countdown])
+        else:
+            spam += first_deposit + (first_deposit * high[countdown])
+            end_deposit = spam + (spam * high[countdown])
+    return end_deposit
+
+
+print(get_deposit_sum(5000, 24))
+print(get_deposit_sum(50000, 12))
+print(get_deposit_sum(500000, 24))
