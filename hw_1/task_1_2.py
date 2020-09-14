44da471362e9bf86a498e3667b71b3d895607e92
Michel Koyang-Kuleshov <michelkoyangkuleshov@gmail.com>
2020-09-14 20:12:01 +0300

    Done task 1.2

--- hw_1/task_1_2.py
+++ hw_1/task_1_2.py
@@ -10,3 +10,27 @@ def print_directory_contents(sPath):
 нельзя. Данная задача показывает ваше умение работать с
 вложенными структурами.
 """
+
+
+import os
+
+
+def print_directory_contents(sPath):
+    try:
+        spam = os.listdir(sPath)
+        if spam:
+            print(f'{sPath}: {spam}')
+    except Exception:
+        pass
+    else:
+        for root in spam:
+            tmp_root = os.path.join(sPath, root)
+            cwd_lst = print_directory_contents(tmp_root)
+            if cwd_lst:
+                print(f'{tmp_root}:{cwd_lst}')
+
+
+print_directory_contents(os.path.join('..'))
+print('*' * 30, 'os.walk', '*' * 30, end='\n')
+for root in os.walk(os.path.join('..')):
+    print(root)
